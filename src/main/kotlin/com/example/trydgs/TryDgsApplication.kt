eaa63bf61e70d26af5afe01395bd36ccc85aa990
h-tachikawa <sle.60e@gmail.com>
2023-06-03 22:31:18 +0900

    build: init SpringBoot project

---
+++ src/main/kotlin/com/example/trydgs/TryDgsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.trydgs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TryDgsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TryDgsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/trydgs/TryDgsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.trydgs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TryDgsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
